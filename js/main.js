245d813add4bd997f0686b2681fe2160abb5070c
ntduncan <nateduncan0@.com>
2021-11-06 21:26:00 -0600

    Week 8 notes

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
-const assignmentList = document.getElementById('assignment-links');
+const assignmentList = document.getElementById("assignment-links");
 const assignments = [
-    {
-        label: 'Week1 Notes',
-        url: 'Week1/index.html'
-    },
-    {
-        label: 'Week4 Notes',
-        url: 'Week4/index.html'
-    },
-    {
-        label: 'Week5 Notes',
-        url: 'Week5/index.html'
-    },
-    {
-        label: 'Todo App',
-        url: 'week6/index.html'
-    },
-    {
-        label: 'Week7 Notes',
-        url: 'Week7/index.html'
-    },
-]
+  {
+    label: "Week1 Notes",
+    url: "Week1/index.html",
+  },
+  {
+    label: "Week4 Notes",
+    url: "Week4/index.html",
+  },
+  {
+    label: "Week5 Notes",
+    url: "Week5/index.html",
+  },
+  {
+    label: "Todo App",
+    url: "week6/index.html",
+  },
+  {
+    label: "Week7 Notes",
+    url: "Week7/index.html",
+  },
+];
 
-assignments.forEach(assignment => {
-    const listItem = document.createElement('li');
-    const link = document.createElement('a');
+assignments.forEach((assignment) => {
+  const listItem = document.createElement("li");
+  const link = document.createElement("a");
 
-    link.textContent = assignment.label;
-    link.href = assignment.url;
+  link.textContent = assignment.label;
+  link.href = assignment.url;
 
-    listItem.appendChild(link); 
-    assignmentList.appendChild(listItem);
-})
+  listItem.appendChild(link);
+  assignmentList.appendChild(listItem);
+});
